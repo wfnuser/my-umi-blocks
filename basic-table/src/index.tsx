317dba022bf93090819eedaf1553444cf97c1890
wfnuser <wfnuser@126.com>
2020-03-01 21:08:12 +0800

    feat: add a basic table block

---
+++ basic-table/src/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styles from './index.less';
+import { Table, Divider } from 'antd';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: record => (
+      <span>
+        <a>Invite {record.name}</a>
+        <Divider type="vertical" />
+        <a>Delete</a>
+      </span>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
+];
+
+export default () => (
+  <div className={styles.container}>
+    <div id="components-table-demo-basic">
+      <Table columns={columns} dataSource={data} />
+    </div>
+  </div>
+);
